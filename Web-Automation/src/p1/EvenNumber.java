6513a894bca1ac339e6ba850d75e4fcd5ee31b41
meghashreepradeep <smeghashree15@gmail.com>
2023-01-02 17:27:59 +0530

    Even number

---
+++ Web-Automation/src/p1/EvenNumber.java
@@ -0,0 +1,23 @@
+package p1;
+
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		{
+			for(int i=200;i>=100;i--)
+			{
+				
+			
+				if(i%2==0)
+					
+			{
+				System.out.println(i);
+			}
+		
+		}
+
+		}
+	}
+}
+
+	
